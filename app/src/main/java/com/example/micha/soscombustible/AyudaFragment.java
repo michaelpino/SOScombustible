0a76c6f2001b346b88e957cc99b4e5f58226cff5
litrox <zorlak.x@gmail.com>
2017-06-05 20:36:13 -0400

    Current location (In ayuda fragment)

--- app/src/main/java/com/example/micha/soscombustible/AyudaFragment.java
+++ app/src/main/java/com/example/micha/soscombustible/AyudaFragment.java
@@ -1,13 +1,24 @@
 package com.example.micha.soscombustible;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,7 +28,9 @@ import android.view.ViewGroup;
  * Use the {@link AyudaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AyudaFragment extends Fragment {
+public class AyudaFragment extends Fragment
+        implements  GoogleApiClient.OnConnectionFailedListener,
+                    GoogleApiClient.ConnectionCallbacks {
     /*// TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,6 +41,16 @@ public class AyudaFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;*/
+
+    private static final String LOGTAG = "android-localizacion";
+
+    private static final int PETICION_PERMISO_LOCALIZACION = 101;
+
+    private GoogleApiClient apiClient;
+
+    private TextView lblLatitud;
+    private TextView lblLongitud;
+    private ToggleButton btnActualizar;
 
     public AyudaFragment() {
         // Required empty public constructor
@@ -58,13 +81,100 @@ public class AyudaFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }*/
+
+        apiClient = new GoogleApiClient.Builder(getActivity())
+                .enableAutoManage(getActivity(), this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ayuda, container, false);
+        View view =  inflater.inflate(R.layout.fragment_ayuda, container, false);
+
+        lblLatitud = (TextView) view.findViewById(R.id.lblLatitud);
+        lblLongitud = (TextView) view.findViewById(R.id.lblLongitud);
+        btnActualizar = (ToggleButton) view.findViewById(R.id.btnActualizar);
+
+        return view;
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        //Se ha producido un error que no se puede resolver automáticamente
+        //y la conexión con los Google Play Services no se ha establecido.
+
+        Log.e(LOGTAG, "Error grave al conectar con Google Play Services");
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //Conectado correctamente a Google Play Services
+
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PETICION_PERMISO_LOCALIZACION);
+        } else {
+
+            Location lastLocation =
+                    LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+            //Toast.makeText(getActivity(),String.valueOf(lastLocation.getLatitude()), Toast.LENGTH_LONG);
+            updateUI(lastLocation);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        //Se ha interrumpido la conexión con Google Play Services
+
+        Log.e(LOGTAG, "Se ha interrumpido la conexión con Google Play Services");
+    }
+
+    private void updateUI(Location loc) {
+        if (loc != null) {
+            lblLatitud.setText("Latitud: " + String.valueOf(loc.getLatitude()));
+            lblLongitud.setText("Longitud: " + String.valueOf(loc.getLongitude()));
+        } else {
+            lblLatitud.setText("Latitud: (desconocida)");
+            lblLongitud.setText("Longitud: (desconocida)");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PETICION_PERMISO_LOCALIZACION) {
+            if (grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                //Permiso concedido
+
+                @SuppressWarnings("MissingPermission")
+                Location lastLocation =
+                        LocationServices.FusedLocationApi.getLastLocation(apiClient);
+
+                updateUI(lastLocation);
+
+            } else {
+                //Permiso denegado:
+                //Deberíamos deshabilitar toda la funcionalidad relativa a la localización.
+
+                Log.e(LOGTAG, "Permiso denegado");
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        apiClient.stopAutoManage(getActivity());
+        apiClient.disconnect();
     }
 
     /*// TODO: Rename method, update argument and hook method into UI event
